6b76a0d29cafb52710bf42f0cc4fef0e0d9be436
Okokon Christopher Ntuk <31165216+chrix95@users.noreply.github.com>
2020-05-23 14:43:12 +0100

    Modification based on deployment

--- resources/js/services/Api.js
+++ resources/js/services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // create a new axios instance
 const instance = axios.create({
-  baseURL: `http://127.0.0.1:8000/api/v1/`
+  baseURL: `/api/v1/`
 });
 
 export default () => instance;
